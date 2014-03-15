c171f3121c14d62504f8d06898e4474f81b58868
dasverrisson <dasverrisson@gmail.com>
2014-03-15 16:11:45 +0000

    Basic template work in place
    needs more work though. loads more even.

--- app/scripts/controllers/template.js
+++ app/scripts/controllers/template.js
@@ -14,16 +14,43 @@ angular.module('exquisiteEvalApp')
       };
 
     $scope.question = {
-        text: ''
+        ID: 0,
+        TextIS: '',
+        TextEN: '',
+        ImageURL:'',
+        Type: '',
+        Answers: []
       };
 
+    $scope.answer = {
+        ID: 0,
+        TextIS: '',
+        TextEN: '',
+        ImageURL: '',
+        Weight: 1
+      };
+
+    $scope.qhandle = {
+      order: 0,
+      teacherOrCourseQ: 'course'
+    };
+
     $scope.vm = {
-        displayQ: false
+        displayQ: false,
+        displayA: false
       };
 
     $scope.addingQuestion = function() {
         $scope.vm.displayQ = true;
       };
+
+    $scope.displayA = function() {
+      $scope.vm.displayA = true;
+    };
+
+    $scope.undisplayA = function() {
+      $scope.vm.displayA = false;
+    };
 
     $scope.addQuestion = function() {
         $scope.template.CourseQuestions.push($scope.question);
@@ -31,14 +58,16 @@ angular.module('exquisiteEvalApp')
       };
 
     $scope.tgetTemplate = function() {
-        console.log('Hi!');
         EvalBackend.getTemplate(1).then(function(data) {
             $scope.template = data;
           });
       };
 
     $scope.postTemplate = function() {
-
-    };
+        EvalBackend.addTemplate($scope.template).then(function(data) {
+          console.log('Some kind of success! Maybe. Kinda. OK.');
+          console.log(data);
+        });
+      };
 
   }]);
