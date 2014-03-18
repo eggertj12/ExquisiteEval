b22ed29a3b04c01a11bd079943c2cee15a5a9362
dasverrisson <dasverrisson@gmail.com>
2014-03-18 18:16:06 +0000

    testing for template

--- test/spec/controllers/template.js
+++ test/spec/controllers/template.js
@@ -30,11 +30,13 @@ describe('Controller: TemplateCtrl', function () {
       $scope: scope
     });
 
+    EvalState = $injector.get('EvalState');
+
   }));
 
   it('createNewTemplate should call two tested functions', function () {
     scope.createNewTemplate();
-    scope.apply();
+    scope.$apply();
     expect(scope.template.TitleIS).toBe('');
   });
 
@@ -45,15 +47,26 @@ describe('Controller: TemplateCtrl', function () {
     expect(scope.template.TitleEN).toBe('');
     expect(scope.template.IntroTextIS).toBe('');
     expect(scope.template.IntroTextEN).toBe('');
-    expect(scope.template.CourseQuestions).toBe([]);
-    expect(scope.template.TeacherQuestions).toBe([]);
+    expect(scope.template.CourseQuestions.length).toBe(0);
+    expect(scope.template.TeacherQuestions.length).toBe(0);
   });
 
-  it('addAnAnswer should set this boolean to true', function() {
-    scope.addAnAnswer();
+  it('cleanQuestion should leave scope.question blank', function() {
+    scope.question.ID = 1;
+    scope.cleanQuestion();
     scope.$apply();
-    expect(scope.vm.displayNewA).toBe('');
+
+    expect(scope.question.ID).toBe(0);
   });
+
+  it('cleanAnswers should leave scope.answer blank', function() {
+    scope.answer.ID = 1;
+    scope.cleanAnswers();
+    scope.$apply();
+
+    expect(scope.answer.ID).toBe(0);
+  });
+
 
   if('addAnswer should add the answer to the answer holder', function() {
     scope.answer.ID = 3;
@@ -76,7 +89,7 @@ describe('Controller: TemplateCtrl', function () {
     scope.showTemplates();
     scope.$apply();
     // Maybe add some meat here, more than just the basic mockbackend
-    expect(scope.vm.templates[0].ID).toBe(1);
+    expect(scope.vm.templates.length).toBe(1);
 
     expect(scope.vm.displayTlist).toBe(true);
     expect(scope.vm.displayT).toBe(false);
@@ -97,7 +110,7 @@ describe('Controller: TemplateCtrl', function () {
     scope.$apply();
 
     expect(scope.template.ID).toBe(9);
-    expect(scope.template.TextIS).toBe('Jahérnahér!');
+    expect(scope.template.TextIS).toBe('Jahérnahér');
     expect(scope.template.TextEN).toBe('What up! mah');
     expect(scope.template.IntroTextIS).toBe('Hvað er að gerast hérna?');
     expect(scope.template.IntroTextEN).toBe('yo yo yo yo yo');
@@ -108,12 +121,12 @@ describe('Controller: TemplateCtrl', function () {
     expect(scope.vm.displayT).toBe(false);
   });
 
-  it('displaSpecificTemplate should talk with the Backend and set some booleans',function() {
-    scope.editSpecificTemplate(9);
+  it('displaySpecificTemplate should talk with the Backend and set some booleans',function() {
+    scope.displaySpecificTemplate(9);
     scope.$apply();
 
     expect(scope.template.ID).toBe(9);
-    expect(scope.template.TextIS).toBe('Jahérnahér!');
+    expect(scope.template.TextIS).toBe('Jahérnahér');
     expect(scope.template.TextEN).toBe('What up! mah');
     expect(scope.template.IntroTextIS).toBe('Hvað er að gerast hérna?');
     expect(scope.template.IntroTextEN).toBe('yo yo yo yo yo');
@@ -121,15 +134,98 @@ describe('Controller: TemplateCtrl', function () {
 
     expect(scope.vm.edit).toBe(false);
     expect(scope.vm.displayTlist).toBe(false);
-    expect(scope.vm.displayT).toBe(true);    
+    expect(scope.vm.displayT).toBe(true);
+  });
+
+  it('confirmMultipleChoiceA should set a boolean', function() {
+    scope.confirmMultipleChoiceA();
+    scope.$apply();
+
+    expect(scope.vm.canConfirmMultipleChoiceA).toBe(false);
+  });
+
+  it('confirmMultipleChoiceA should set a boolean based on logic', function() {
+    scope.multipleChoiceAnswer.answers.push(scope.answer);
+    scope.multipleChoiceAnswer.answers.push(scope.answer);
+    scope.confirmMultipleChoiceA();
+    scope.$apply();
+
+    expect(scope.vm.canConfirmMultipleChoiceA).toBe(true);
   });
 
 
-  it('addingQuestion should set a boolean', function() {
+  it('addingQuestion should set a boolean based on logic', function() {
     scope.addingQuestion();
     scope.$apply();
 
     expect(scope.vm.displayQ).toBe(true);
+  });
+
+  it('qTypeText should set some booleans', function() {
+    scope.qTypeText();
+    scope.$apply();
+
+    expect(scope.vm.displaySingleChoiceA).toBe(false);
+    expect(scope.vm.displayMultipleChoiceA).toBe(false);
+  });
+
+  it('qTypeSingle should set some booleans', function() {
+    scope.qTypeSingle();
+    scope.$apply();
+
+    expect(scope.vm.displaySingleChoiceA).toBe(true);
+    expect(scope.vm.displayMultipleChoiceA).toBe(false);
+  });
+
+  it('qTypeMultiple should set some booleans and add some answers', function(){
+    scope.qTypeMultiple();
+    scope.$apply();
+
+    expect(scope.vm.displaySingleChoiceA).toBe(false);
+    expect(scope.vm.displayMultipleChoiceA).toBe(true);
+    expect(scope.multipleChoiceAnswer.answers.length).toBe(2);
+  });
+
+  it('addChoice should write some stuff into an array', function() {
+    scope.answer.ID = 1;
+    scope.addChoice();
+    scope.$apply();
+
+    expect(scope.multipleChoiceAnswer.answers.length).toBe(1);
+  });
+
+  it('removeChoice should remove a choice', function() {
+    scope.answer.ID = 3;
+    scope.addChoice();
+    scope.answer.ID = 4;
+    scope.addChoice();
+    scope.removeChoice(1);
+    scope.$apply();
+
+    expect(scope.multipleChoiceAnswer.answers.length).toBe(1);
+  });
+
+  it('addMultipleChoiceAnswer should add items to an array', function() {
+    scope.qTypeMultiple();
+    scope.addMultipleChoiceAnswer();
+    scope.$apply();
+
+    expect(scope.question.Answers.length).toBe(2);
+  });
+
+  it('addSingleChoiceAnswer should add an item to an array', function() {
+    scope.qTypeSingle();
+    scope.addSingleChoiceAnswer();
+    scope.$apply();
+
+    expect(scope.question.Answers.length).toBe(1);
+  });
+
+  it('abortAnswer should call a tested function and set some booleans', function() {
+    scope.abortAnswer();
+    scope.$apply();
+
+    expect(scope.vm.displaySingleChoiceA).toBe(false);
   });
 
   it('displayA should set a boolean', function() {
@@ -139,15 +235,16 @@ describe('Controller: TemplateCtrl', function () {
     expect(scope.vm.displayA).toBe(true);
   });
 
-  it('unDisplayA should set a boolean', function() {
-    scope.unDisplayA();
+  it('undisplayA should set a boolean', function() {
+    scope.vm.displayA = true;
+    scope.undisplayA();
     scope.$apply();
 
     expect(scope.vm.displayA).toBe(false);
   });
 
   it('addQuestion should do some logic, add a question to the quesiton holder and set a boolean', function() {
-    scope.qhandle.teacherOrCourseQuestion = 'course';
+    scope.qhandle.teacherOrCourseQ = 'course';
     scope.question.TextIS = 'Drullumall';
 
     scope.addQuestion();
@@ -159,13 +256,48 @@ describe('Controller: TemplateCtrl', function () {
   });
 
   it('addQuestion definitely needs logic coverage', function() {
-    scope.qhandle.teacherOrCourseQuestion = 'teacher';
-    scope.question.TextEN = "hangon";
+    scope.qhandle.teacherOrCourseQ = 'teacher';
+    scope.question.TextIS = 'Drullumall';
+    scope.question.TextEN = 'hangon';
 
     scope.addQuestion();
     scope.$apply();
 
     expect(scope.template.TeacherQuestions[0].TextEN).toBe('hangon');
+  });
+
+  it('abortQuestion should call a tested function and set a boolean', function() {
+    scope.abortQuestion();
+    scope.$apply();
+
+    expect(scope.vm.displayQ).toBe(false);
+  });
+
+  it('removeCourseQuestion should remove an item from an array', function() {
+    scope.qhandle.teacherOrCourseQ = 'course';
+    scope.question.TextIS = 'Drullumall';
+    scope.question.TextEN = 'hangon';
+
+    scope.addQuestion();
+    
+    scope.removeCourseQuestion(0);
+
+    scope.$apply();
+    expect(scope.template.CourseQuestions.length).toBe(0);
+
+  });
+
+  it('removeTeacherQuestion should remove and item from an array', function() {
+    scope.qhandle.teacherOrCourseQ = 'teacher';
+    scope.question.TextIS = 'Drullumall';
+    scope.question.TextEN = 'hangon';
+
+    scope.addQuestion();
+    
+    scope.removeTeacherQuestion(0);
+
+    scope.$apply();
+    expect(scope.template.TeacherQuestions.length).toBe(0);
   });
 
   it('tgetTemplate is an unused dummy function which should be deleted, but I will write a test for it rather than delete it', function() {
@@ -193,6 +325,15 @@ describe('Controller: TemplateCtrl', function () {
 
     expect(scope.vm.edit).toBe(false);
     
+  });
+
+  it('makeEvalFromThisTemplate should set EvalState and change location', function() {
+    scope.makeEvalFromThisTemplate(6);
+    scope.$apply();
+
+    expect(EvalState.TemplateID).toBe(6);
+    expect($location.url()).toBe('/evaluation');
+
   });
 
   
